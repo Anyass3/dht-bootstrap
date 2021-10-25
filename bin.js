c3fdf88d3423c21772bb88f8bd03d8964cb21f9f
Anyass3 <nyassabu@gmail.com>
2021-10-25 07:15:39 +0000

    added ports argv

--- bin.js
+++ bin.js
@@ -4,15 +4,56 @@ const DHT = require('@hyperswarm/dht');
 const path = require('path');
 const fs = require('fs');
 const child_process = require('child_process');
+const DEFAULT_PORTS = [10001, 10002];
+const ports = [];
+
+if (process.argv.includes('--ports'))
+  for (let i = 3; i < process.argv.length; i++) ports.push(parseInt(process.argv[i]));
 
 const configJsonPath = path.resolve(path.join(process.argv[1], '../', './config.json'));
+
+let config = { ports: ports.length >= 2 ? ports : DEFAULT_PORTS, env: {} };
+
 if (!fs.existsSync(configJsonPath)) {
-  fs.writeFileSync(configJsonPath, JSON.stringify({ ports: [10001, 10002], env: {} }));
-}
-const config = JSON.parse(fs.readFileSync(configJsonPath, { encoding: 'utf-8' }));
+  fs.writeFileSync(configJsonPath, JSON.stringify(config, undefined, 2));
+} else if (ports.length >= 2) {
+  config = JSON.parse(fs.readFileSync(configJsonPath, { encoding: 'utf-8' }));
+  config.ports = ports;
+  fs.writeFileSync(configJsonPath, JSON.stringify(config, undefined, 2));
+} else config = JSON.parse(fs.readFileSync(configJsonPath, { encoding: 'utf-8' }));
+
+const child = (exe, args, env) => {
+  const child = child_process.spawn(exe, args, {
+    detached: true,
+    stdio: ['ignore', 'ignore', 'ignore'],
+    env,
+  });
+  child.unref();
+  return child;
+};
+
+const daemon = () => {
+  if (process.env.__daemon) {
+    return process.pid;
+  }
+
+  if (config.env.PID) {
+    // kill previous daemon before starting a new one
+    child('kill', ['-9', config.env.PID], process.env);
+  }
+  const args = [].concat(process.argv);
+  const node = args.shift();
+  const env = { __daemon: true, ...process.env };
+  const { pid } = child(node, args, env);
+  config.env.PID = pid;
+  fs.writeFileSync(configJsonPath, JSON.stringify(config, undefined, 2));
+};
+
+daemon(); //starts the daemon
+
 const getBootstrap = ({ address, port }) => ({ host: address, port });
 (async () => {
-  //bootstrap should be empty in order have a fully private dht
+  // bootstrap should be empty in order to have a fully private dht
   const bootstrapper1 = DHT.bootstrapper(config.ports[0], { ephemeral: true, bootstrap: [] });
 
   await bootstrapper1.ready();
@@ -28,37 +69,15 @@ const getBootstrap = ({ address, port }) => ({ host: address, port });
     await bootstrapper.ready();
     bootstraps.push(getBootstrap(bootstrapper.address()));
   }
+
   config.ports = bootstraps.map((bootstrap) => bootstrap.port);
-  fs.writeFileSync(configJsonPath, JSON.stringify(config));
+  fs.writeFileSync(configJsonPath, JSON.stringify(config, undefined, 2));
+
+  console.log('\nBootstrap nodes listening on:\n');
+  for (const bootstrap of bootstraps) {
+    console.log('\t', bootstrap);
+  }
+  console.log('\n');
+  // if we are in daemon mode don't kill process
+  if (!process.env.__daemon) process.exit();
 })();
-
-const child = (exe, args, env) => {
-  var child = child_process.spawn(exe, args, {
-    detached: true,
-    stdio: ['ignore', 'ignore', 'ignore'],
-    env,
-  });
-  child.unref();
-  return child;
-};
-
-const daemon = () => {
-  console.log('starting bootstrap nodes as a daemon');
-  if (process.env.__daemon) {
-    return process.pid;
-  }
-  process.env.__daemon = true;
-
-  if (config.env.PID) {
-    child('kill', ['-9', config.env.PID], process.env);
-  }
-  const args = [].concat(process.argv);
-  const node = args.shift();
-  const env = process.env;
-  const { pid } = child(node, args, env);
-  config.env.PID = pid;
-  fs.writeFileSync(configJsonPath, JSON.stringify(config));
-  return process.exit();
-};
-
-daemon();
